923576572da313eeb5ea1ae7201870f962fe4282
RoseAlice2018 <1505243015@qq.com>
2021-06-24 18:13:30 +0800

    update 6.24 实验三

---
+++ exp3/B+TreeNode.h
@@ -0,0 +1,71 @@
+#ifndef BTREENODE_H
+#define BTREENODE_H
+
+/*
+    B+Tree 节点
+    K是码，E是记录
+    E类型必须实现函数(K key())定义
+*/
+#include<deque>
+namespace BATreeNode{
+    //内部节点
+    template<typename K,typename E>
+    class BAddTreeNode{
+        public:
+            //只能作为根节点创建
+            explicit BAddTreeNode();//默认构造函数，创建一个空Btree，初始化时有0个关键码及一个空孩子分支
+            ~BAddTreeNode();//析构不释放孩子内存，仅仅释放节点e内存
+            bool isLeaf()const;//判断节点是否为叶子节点
+        public:
+            BAddTreeNode<K,E>* parent;//父节点
+            std::deque<K>key;//关键码
+            std::deque<BAddTreeNode<K,E>*>child;//孩子分支，总比关键字码多1
+    };
+    template<typename K,typename E>
+    inline BAddTreeNode<K,E>::BAddTreeNode()
+    {
+        parent = nullptr;
+        child.insert(child.end(),nullptr);//默认孩子为空
+    }
+
+    template<typename K,typename E>
+    inline BAddTreeNode<K,E>::~BAddTreeNode()
+    {
+
+    }
+    template<typename K,typename E>
+    inline bool BAddTreeNode<K,E>::isLeaf()const
+    {
+        return !child[0];
+    }
+    //叶子节点
+    template<typename K,typename E>
+    class BAddTreeLeafNode:public BAddTreeNode<K,E>
+    {
+        public:
+            //只能作为根节点创建
+            explicit BAddTreeLeafNode();//默认构造函数 创建一个空B-tree,初始化时有0个关键码及1个空孩子分支
+            ~BAddTreeLeafNode();
+        public:
+            std::deque<E*>e;//记录
+            BAddTreeLeafNode<K,E>*last,*next;//链表前后
+    };
+
+    template<typename K,typename E>
+    inline BAddTreeLeafNode<K,E>::BAddTreeLeafNode()
+    {
+        last = nullptr;
+        next = nullptr;
+    }
+    template<typename K,typename E>
+    inline BAddTreeLeafNode<K,E>::~BAddTreeLeafNode()
+    {
+        int _s = e.size();
+        for(int i=0;i < _s; ++i)
+        {
+            delete e[i],e[i] = nullptr;
+        }        
+        e.resize(0);
+    }
+};
+#endif
